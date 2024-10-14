30b7d89f710000d76bc8c7e7ae8936e45bd619ed
Joshua Smith <mtgmirage@protonmail.com>
2024-10-14 09:37:28 -0500

    Update facilities.js

--- scripts/facilities.js
+++ scripts/facilities.js
@@ -8,7 +8,7 @@ export const facilityDropdown = async () => {
 
     return `
         <div>
-            <select id="facility-list">
+            <select id="facility">
                 <option value="0">Please Choose a facility</option>
                     ${data.map(facility =>
                         `<option value="${facility.id}">${facility.name}</option>`
@@ -20,7 +20,7 @@ export const facilityDropdown = async () => {
 
 //needs setFacilityId... I put these in my transState module in the last project
 export const handleFacilityChoice = (changeEvent) => {
-    if (changeEvent.target.id === "facility-list") {
+    if (changeEvent.target.id === "facility") {
         const convertedToInteger = (parseInt(changeEvent.target.value))
         setFacilityId(convertedToInteger)
     }
