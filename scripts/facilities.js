2b35c1a977a41e7ce0a45980ae7e2a2faa676739
Rosemary Chadwick <rrchadwick@hotmail.com>
2024-10-14 09:02:43 -0500

    governoer dropdown and handleclick module made

---
+++ scripts/facilities.js
@@ -0,0 +1,27 @@
+
+
+export const facilityDropdown = async () => {
+    const response = await fetch("http://localhost:8088/facilities")
+    const data = await response.json()
+
+    document.addEventListener("change", handleFacilityChoice)
+
+    return `
+        <div>
+            <select id="facility-list">
+                <option value="0">Please Choose a facility</option>
+                    ${data.map(facility =>
+                        `<option value="${facility.id}">${facility.name}</option>`
+                    ).join('')}
+            </select>
+        </div>
+    `
+}
+
+//needs setFacilityId... I put these in my transState module in the last project
+export const handleFacilityChoice = (changeEvent) => {
+    if (changeEvent.target.id === "facility-list") {
+        const convertedToInteger = (parseInt(changeEvent.target.value))
+        setFacilityId(convertedToInteger)
+    }
+}
